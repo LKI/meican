aa726c939e3f6a3135b865b737431da95dc2de3b
Lirian Su <liriansu@gmail.com>
2017-10-17 13:55:52 +0800

    feat: update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,17 @@ from setuptools import setup
 
 import meican
 
+
+def get_long_description(readme_file='README.md'):
+    with open(readme_file, 'rb') as f:
+        return f.read().decode('utf-8')
+
+
 setup(
     name='meican',
     version=meican.__version__,
     description='UNOFFICIAL meican command line / sdk',
+    long_description=get_long_description(),
     author='Lirian Su',
     author_email='liriansu@gmail.com',
     url='https://github.com/hui-z/meican',
